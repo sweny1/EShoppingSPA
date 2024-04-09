2fed9bffc1f5d584f582b7196601a6bc43139899
sweny <sweny091@gmail.com>
2024-04-09 14:23:15 -0600

    Eshopping SPA initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'EShoppingSPA';
+export class AppComponent implements OnInit{
+  title = 'EShopping';
+
+  constructor(private http : HttpClient){ }
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:9010/Catalog/GetProductsByBrandName/Adidas').subscribe({
+      next: respose => console.log(respose),
+      error: Error => console.log(Error),
+      complete: () => console.log("all products fetched successfully.")
+  });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
